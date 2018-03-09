85c58747cf6fbd8d63da292b86adc0a063c17b33
WhiteProtoss <haun24ne@gmail.com>
2018-03-09 23:28:08 +0900

    There is the reason why they said to me "you have to hear girl's say"

---
+++ WINAPIStudying_02/WINAPIStudying_02/DrawText.c
@@ -0,0 +1,71 @@
+#include <Windows.h>
+#include <TCHAR.H>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS WndClass;
+
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;//프로시저 함수
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDC_ARROW);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = _T("Window1");
+
+	RegisterClass(&WndClass);
+
+	hwnd = CreateWindow(_T("Window1"), _T("Window Title Name"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return  (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT 소연;
+	switch (iMsg)
+	{
+	case WM_CREATE://윈도우가 켜졌을때
+		break;
+
+	case WM_PAINT://윈도우가 화면에 나타날때....
+		hdc = GetDC(hwnd);
+		TextOut(hdc, 500,500, _T("나는 너를 좋아한다 소연아"), _tcslen(_T("나는 너를 좋아한다 소연아")));
+		ReleaseDC(hwnd, hdc);
+
+		소연.bottom = 200;
+		소연.right = 200;
+		소연.top = 100;
+		소연.left = 100;
+
+		hdc = BeginPaint(hwnd, &ps);
+		DrawText(hdc, _T("나는 19살이다."), _tcslen(_T("나는 19살이다.")), &소연, DT_SINGLELINE|DT_CENTER);
+		EndPaint(hwnd, &ps);
+		break;
+
+	case WM_DESTROY://윈도우가 꺼질때
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
